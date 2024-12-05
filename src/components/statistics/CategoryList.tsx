33a241fc12abd2849337452df1dd2ac488a61455
Jakub Cieślik <kuba8172@gmail.com>
2024-12-05 10:22:11 +0100

    added overflow

--- src/components/statistics/CategoryList.tsx
+++ src/components/statistics/CategoryList.tsx
@@ -8,11 +8,11 @@ async function CategoryList() {
   const transactions = await getCategoriesWithTransactions();
 
   return (
-    <div className="w-full xl:w-1/3 bg-stone-700 shadow-md rounded-md p-3 ">
+    <div className="w-full xl:w-1/3 bg-stone-700 shadow-md rounded-md p-3 overflow-y-auto max-h-[300px] ">
       <div className="w-full flex flex-col gap-y-2 justify-center items-center">
         <h2 className="font-bold">Categories</h2>
 
-        <div className="w-full flex flex-col gap-2">
+        <div className="w-full flex flex-col gap-2 ">
           {transactions.map((item) => (
             <CategoryItem key={item.category.id} item={item} />
           ))}
@@ -29,7 +29,7 @@ const CategoryItem = ({ item }: { item: { category: Category; transactions: Tran
     href={`/category/${item.category.id}`}
     className="flex flex-row justify-between hover:bg-white/10 py-1 px-1 rounded-xl cursor-pointer"
   >
-    <div className="flex gap-2 items-center justify-center max-w-[30%] ">
+    <div className="flex gap-2 items-center justify-center max-w-[40%] ">
       <div className="min-w-[8px] h-[8px] rounded-full" style={{ backgroundColor: item.category.color }} />
       <div className="whitespace-nowrap overflow-hidden capitalize">{item.category.name}</div>
     </div>
